58d3c698a9e021524f63e2efb32130d845292fd7
nishihara.iwao <65902851+u2soft@users.noreply.github.com>
2020-05-31 21:25:41 +0900

    勉強用です。更新しました

---
+++ DeepCopy/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeepCopy
+{
+
+    public class 選手情報
+    {
+
+        public class 選手
+        {
+            string 名前 { get; set; }
+            int 年齢 { get; set; }
+            public 選手(string name, int age)
+            {
+                名前 = name;
+                年齢 = age;
+            }
+            public 選手(選手 p)
+            {
+                名前 = p.名前;
+                年齢 = p.年齢;
+            }
+        }
+
+        public class 所属
+        {
+            string 名前 { get; set; }
+            int 番号 { get; set; }
+            public 所属(string name, int number)
+            {
+                名前 = name;
+                番号 = number;
+            }
+
+            public 所属(所属 s)
+            {
+                名前 = s.名前;
+                番号 = s.番号;
+            }
+        }
+
+        public 選手 player { get; set; }
+        public 所属 info { get; set; }
+
+        public 選手情報(選手 playerInfo,所属 clubInfo)
+        {
+            player = playerInfo;
+            info = clubInfo;
+        }
+        public 選手情報(選手情報 i)
+        {
+            player = i.player;
+            info = i.info;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tourdefranc1998 = new Dictionary<int, 選手情報>();
+            tourdefranc1998.Add(1, new 選手情報(new 選手情報.選手(name: "マルコ・パンターニ", age:30), new 選手情報.所属("ビアンキ", 19981)));
+            tourdefranc1998.Add(2, new 選手情報(new 選手情報.選手(name: "ヤン・ウルリッヒ", 　age:20), new 選手情報.所属("テレコム", 19982)));
+            tourdefranc1998.Add(3, new 選手情報(new 選手情報.選手(name: "ボビー・ジュリック", age:25), new 選手情報.所属("コフィディス", 19983)));
+
+            var tourdefranc2018 = new Dictionary<int, 選手情報>();
+            tourdefranc2018.Add(1, new 選手情報(new 選手情報.選手(name: "ゲラント・トーマス", age: 30), new 選手情報.所属("チームスカイ",20181)));
+            tourdefranc2018.Add(2, new 選手情報(new 選手情報.選手(name: "トム・デュムラン", age: 20), new 選手情報.所属("チーム・サンウェブ", 20182)));
+            tourdefranc2018.Add(3, new 選手情報(new 選手情報.選手(name: "クリス・フルーム", age: 25), new 選手情報.所属("チームスカイ", 20183)));
+
+
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Program
+    {
+        class Images
+        {
+            public class Point
+            {
+                public int x;
+                public int y;
+                public int z;
+            }
+
+            public class Data
+            {
+                public int d1;
+                public int d2;
+            }
+
+            public Point point = new Point(); //大事！！
+            public Data data = new Data();//大事！！
+        };
+
+        static void Main(string[] args)
+        {
+            void dump(Dictionary<int, List<Images>> dics)
+            {
+                foreach(var dic in dics)
+                {
+                    var key = dic.Key;
+                    var lists = dic.Value;
+
+                    Console.Write($"key = {key}\n");
+                    foreach(var item in lists.Select((value, index) => new {value,index}))
+                    {
+                        Console.Write($"list[{item.index}],point.x = {item.value.point.x}\n");
+                        Console.Write($"list[{item.index}],point.y = {item.value.point.y}\n");
+                        Console.Write($"list[{item.index}],point.z = {item.value.point.z}\n");
+
+                        Console.Write($"list[{item.index}],data.d1 = {item.value.data.d1}\n");
+                        Console.Write($"list[{item.index}],data.d2 = {item.value.data.d2}\n");
+
+                    }
+
+                }
+            }
+
+            var dicX = new Dictionary<int, List<Images>>();
+            var dicY = new Dictionary<int, List<Images>>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                List<Images> lists = new List<Images>();
+                for (int j = 0; j < 10; j++)
+                {
+                    var aaa = new Images();
+                    aaa.point.x = j;
+                    aaa.point.y = j;
+                    aaa.point.z = j;
+                    aaa.data.d1 = j;
+                    aaa.data.d2 = j;
+                    lists.Add(aaa);
+                }
+                dicX.Add(i, lists);
+            }
+
+            dump(dicX);
+
+            for (int i = 0; i < 3; i++)
+            {
+                List<Images> lists = new List<Images>();
+                for (int j = 100; j < 105; j++)
+                {
+                    var aaa = new Images();
+                    aaa.point.x = j;
+                    aaa.point.y = j;
+                    aaa.point.z = j;
+                    aaa.data.d1 = j;
+                    aaa.data.d2 = j;
+                    lists.Add(aaa);
+                }
+                dicY.Add(i, lists);
+            }
+            dump(dicY);
+
+
+
+        }
+    }
+}
